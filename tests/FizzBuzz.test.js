a7d08f003e315c87339677ba8e44f3c9c7f1af01
Angela <antunezangela89@gmail.com>
2023-12-20 09:07:32 +0100

    Add files

---
+++ tests/FizzBuzz.test.js
@@ -0,0 +1,23 @@
+//Imports
+const {expect} = require("@jest/globals");
+const { describe } = require("yargs");
+
+/**
+Feature para saber cuándo un número es primo de 3 y 5
+    1. Cuando el usuario digite un número divisible por 3, devuelve Fizz,
+    2. Cuando el usuario digite un número divisible por 5, devuelve Buzz,
+    3. Cuando el usuario digite un número divisible por 3 y 5, devuelve FizzBuzz,
+    4. Cuando el usuario digite un número que no sea divisible por 3 y 5, devuelve el número
+
+*/
+
+describe('Tests que validan los múltiplos de 3 y 5', () => {
+    test('Test devuelve Fizz cuando es divisible por 3', () => {
+        /*
+         * GHERKIN TEST
+            * GIVEN / ARRANGE: un número
+            * WHEN / ACT: Cuando éste sea divisible por 3
+            * THEN / ASSERT: devuelve Fizz como resultado
+         */
+    })
+})
