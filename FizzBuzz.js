9288384278f7eb9b8c4ccfe36457dc4e612ab5da
Angela <antunezangela89@gmail.com>
2023-12-22 17:26:52 +0100

    First test achieved and delete {describe} from imports

---
+++ FizzBuzz.js
@@ -0,0 +1,9 @@
+class FizzBuzz {
+    validar_primos_3_5(num) {
+        if (num %3 === 0){
+            return "Fizz";
+        }
+    }
+};
+
+module.exports = FizzBuzz;
--- tests/FizzBuzz.test.js
+++ tests/FizzBuzz.test.js
@@ -1,6 +1,6 @@
 //Imports
 const {expect} = require("@jest/globals");
-const { describe } = require("yargs");
+const FizzBuzz = require("../FizzBuzz");
 
 /**
 Feature para saber cuándo un número es primo de 3 y 5
@@ -19,5 +19,37 @@ describe('Tests que validan los múltiplos de 3 y 5', () => {
             * WHEN / ACT: Cuando éste sea divisible por 3
             * THEN / ASSERT: devuelve Fizz como resultado
          */
+        
+        //ARRANGE: preparar el escenario
+        const numero_ingresado = 3;
+        const respuesta_esperada = "Fizz";
+        const fizz = new FizzBuzz(); 
+            //Me estoy creando un objeto de clase, instancio la clase; buscar más sobre esto; creo que es sobre POO
+        
+        //ACT: ejecutar el escenario
+        const respuesta_recibida = fizz.validar_primos_3_5(numero_ingresado);
+
+        //ASSERT: comprobar el escenario
+        expect(respuesta_recibida).toBe(respuesta_esperada);
+    })
+    test('Test devuelve Fizz cuando es divisible por 5', () => {
+        /*
+         * GHERKIN TEST
+            * GIVEN / ARRANGE: un número
+            * WHEN / ACT: Cuando éste sea divisible por 5
+            * THEN / ASSERT: devuelve Fizz como resultado
+         */
+        
+        //ARRANGE: preparar el escenario
+        const numero_ingresado = 3;
+        const respuesta_esperada = "Fizz";
+        const fizz = new FizzBuzz(); 
+            //Me estoy creando un objeto de clase, instancio la clase; buscar más sobre esto; creo que es sobre POO
+        
+        //ACT: ejecutar el escenario
+        const respuesta_recibida = fizz.validar_primos_3_5(numero_ingresado);
+
+        //ASSERT: comprobar el escenario
+        expect(respuesta_recibida).toBe(respuesta_esperada);
     })
 })
